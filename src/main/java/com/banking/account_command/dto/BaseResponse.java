90dff9391c2a7fa316a4f958f37b5dcb1250a8e9
camilitwo <camilo.igv@gmail.com>
2023-12-19 15:09:56 -0300

    Implement CQRS architecture for banking domain
    Introduce event modeling and custom exception handling as part of a Command Query Responsibility Segregation (CQRS) architectural pattern implementation for the banking application. The addition includes creation of abstract classes and interfaces representing events, commands, queries and handlers, command dispatcher, and an infrastructure layer for managing events. Essential custom exceptions for concurrency and aggregate not found have also been introduced.

---
+++ src/main/java/com/banking/account_command/dto/BaseResponse.java
@@ -0,0 +1,15 @@
+package com.banking.account_command.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BaseResponse {
+    private String message;
+}
--- src/main/java/com/banking/account_command/events/AccountClosedEvent.java
+++ src/main/java/com/banking/account_command/events/AccountClosedEvent.java
@@ -1,8 +1,12 @@
 package com.banking.account_command.events;
 
 import com.banking.cqrs_core.events.BaseEvent;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
 
 @Data
 @SuperBuilder
